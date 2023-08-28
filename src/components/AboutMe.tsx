b1c7ff58315392c6d61d273e9127b2990138dd4d
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2023-08-28 22:19:34 +0200

    create content for aboutme

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+
+  return (
+    <>
+        <div className="AboutMe-container">
+
+            <span className="Greeting-text">Hello,</span>             
+                
+            <h3>My name is Jenny Svensson 
+            and I'm a soon to be Frontend Developer 
+            based in Malmö, Sweden.
+            </h3>
+
+            <img 
+                src="src\assets\img\Untitled-1.png" 
+                className="AboutMe-img" 
+                alt="a shiba dog with japanese theme background with sakura and a mountain" />
+        </div>
+    </>
+  )
+}
